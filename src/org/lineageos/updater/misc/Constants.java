LineageOS/android_packages_apps_Updater

Updater: Add property that allows us to downgrade
Change-Id: I6916639f05c52ae198f0d1656deb23ee5186907f

src/org/lineageos/updater/misc/Constants.java
@@ -37,6 +37,7 @@ public final class Constants {
     public static final String PROP_DEVICE = "ro.lineage.device";
     public static final String PROP_NEXT_DEVICE = "ro.updater.next_device";
     public static final String PROP_RELEASE_TYPE = "ro.lineage.releasetype";
+    public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "lineage.updater.allow_downgrading";
     public static final String PROP_UPDATER_URI = "lineage.updater.uri";
 
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";

DerpFest-AOSP/packages_apps_Updater

Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

src/org/lineageos/updater/misc/Constants.java
@@ -38,6 +38,7 @@ public final class Constants {
     public static final String PROP_DEVICE = "ro.aosip.device";
     public static final String PROP_NEXT_DEVICE = "ro.updater.next_device";
     public static final String PROP_RELEASE_TYPE = "ro.aosip.buildtype";
+    public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "aosip.updater.allow_downgrading";
     public static final String PROP_UPDATER_URI = "aosip.updater.uri";
 
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";