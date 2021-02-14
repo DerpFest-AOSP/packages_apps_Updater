6d4cca6c3506e974620e5a4e8648d4451de22b95
Kshitij Gupta <kshitijgm@gmail.com>
2021-02-14 17:21:13 +0530

    Updater: Fix Android Studio build and kill AB-perf
    Change-Id: Ibbb74c7161110f3c4284ff3a76a479195aa39e5a

--- src/org/lineageos/updater/UpdatesActivity.java
+++ src/org/lineageos/updater/UpdatesActivity.java
@@ -416,18 +416,12 @@ public class UpdatesActivity extends UpdatesListActivity {
                 view.findViewById(R.id.preferences_auto_updates_check_interval);
         Switch autoDelete = view.findViewById(R.id.preferences_auto_delete_updates);
         Switch dataWarning = view.findViewById(R.id.preferences_mobile_data_warning);
-        Switch abPerfMode = view.findViewById(R.id.preferences_ab_perf_mode);
         Switch updateRecovery = view.findViewById(R.id.preferences_update_recovery);
-
-        if (!Utils.isABDevice()) {
-            abPerfMode.setVisibility(View.GONE);
-        }
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         autoCheckInterval.setSelection(Utils.getUpdateCheckSetting(this));
         autoDelete.setChecked(prefs.getBoolean(Constants.PREF_AUTO_DELETE_UPDATES, false));
         dataWarning.setChecked(prefs.getBoolean(Constants.PREF_MOBILE_DATA_WARNING, true));
-        abPerfMode.setChecked(prefs.getBoolean(Constants.PREF_AB_PERF_MODE, false));
 
         if (getResources().getBoolean(R.bool.config_hideRecoveryUpdate)) {
             // Hide the update feature if explicitely requested.
@@ -468,8 +462,6 @@ public class UpdatesActivity extends UpdatesListActivity {
                                     autoDelete.isChecked())
                             .putBoolean(Constants.PREF_MOBILE_DATA_WARNING,
                                     dataWarning.isChecked())
-                            .putBoolean(Constants.PREF_AB_PERF_MODE,
-                                    abPerfMode.isChecked())
                             .apply();
 
                     if (Utils.isUpdateCheckEnabled(this)) {
@@ -479,10 +471,6 @@ public class UpdatesActivity extends UpdatesListActivity {
                         UpdatesCheckReceiver.cancelUpdatesCheck(this);
                     }
 
-                    if (Utils.isABDevice()) {
-                        boolean enableABPerfMode = abPerfMode.isChecked();
-                        mUpdaterService.getUpdaterController().setPerformanceMode(enableABPerfMode);
-                    }
                     if (Utils.isRecoveryUpdateExecPresent()) {
                         boolean enableRecoveryUpdate = updateRecovery.isChecked();
                         SystemProperties.set(Constants.UPDATE_RECOVERY_PROPERTY,
--- src/org/lineageos/updater/controller/ABUpdateInstaller.java
+++ src/org/lineageos/updater/controller/ABUpdateInstaller.java
@@ -208,10 +208,6 @@ class ABUpdateInstaller {
             }
         }
 
-        boolean enableABPerfMode = PreferenceManager.getDefaultSharedPreferences(mContext)
-                .getBoolean(Constants.PREF_AB_PERF_MODE, false);
-        mUpdateEngine.setPerformanceMode(enableABPerfMode);
-
         String zipFileUri = "file://" + file.getAbsolutePath();
         mUpdateEngine.applyPayload(zipFileUri, offset, 0, headerKeyValuePairs);
 
@@ -276,10 +272,6 @@ class ABUpdateInstaller {
         mUpdaterController.notifyUpdateChange(mDownloadId);
 
         return true;
-    }
-
-    public void setPerformanceMode(boolean enable) {
-        mUpdateEngine.setPerformanceMode(enable);
     }
 
     public boolean suspend() {
