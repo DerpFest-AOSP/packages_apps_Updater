f2c98053d8dfb240da47e00137e9574af6d7e8f4
Kshitij Gupta <kshitijgm@gmail.com>
2021-02-15 03:20:28 +0530

    Updater: Fix changelog URL get method
    Change-Id: I57001add1ac178ebc06502c3759a2d965f6e0a61

--- src/org/lineageos/updater/misc/Utils.java
+++ src/org/lineageos/updater/misc/Utils.java
@@ -201,9 +201,7 @@ public class Utils {
     }
 
     public static String getChangelogURL(Context context) {
-        String device = SystemProperties.get(Constants.PROP_NEXT_DEVICE,
-                SystemProperties.get(getProjectProp(Constants.PROP_DEVICE)));
-        return context.getString(R.string.changelog_url, device);
+        return context.getString(R.string.changelog_url);
     }
 
     public static void triggerUpdate(Context context, String downloadId) {
