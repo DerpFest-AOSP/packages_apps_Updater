syberia-project/platform_packages_apps_Updater

Add storage permission request to local update
Signed-off-by: DennySPB <dennyspb@gmail.com>
Change-Id: Idd5d9d0ed096691edc2d5fcb5a21e0f5f1522b00

src/org/lineageos/updater/UpdatesActivity.java
@@ -15,12 +15,16 @@
  */
 package org.lineageos.updater;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
+
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.icu.text.DateFormat;
@@ -33,7 +37,9 @@ import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import androidx.core.content.ContextCompat;
 import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -84,6 +90,7 @@ public class UpdatesActivity extends UpdatesListActivity {
     private RotateAnimation mRefreshAnimation;
 
     private static final int READ_REQUEST_CODE = 42;
+    private static final int PERMISSION_REQUEST_CODE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -456,12 +463,62 @@ public class UpdatesActivity extends UpdatesListActivity {
     }
 
     private void performFileSearch() {
+        if (checkStoragePermissions()) {
+            return;
+        }
         Intent chooseFile;
         Intent intent;
         chooseFile = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         chooseFile.setType("application/zip");
         intent = Intent.createChooser(chooseFile, "Choose a file");
         startActivityForResult(intent, READ_REQUEST_CODE);
+    }
+
+    private boolean hasStoragePermission() {
+        int result = checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private boolean checkStoragePermissions() {
+        if (hasStoragePermission()) {
+            return false;
+        }
+
+        final String[] perms = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        requestPermissions(perms, PERMISSION_REQUEST_CODE);
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                                != PackageManager.PERMISSION_GRANTED) {
+                            showMessageOKCancel(getString(R.string.dialog_permissions_title),
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                checkStoragePermissions();
+                                            }
+                                        }
+                                    });
+                        }
+                }
+                break;
+        }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton(getString(R.string.dialog_permissions_ask), okListener)
+                .setNegativeButton(getString(R.string.dialog_permissions_dismiss), null)
+                .create()
+                .show();
     }
 
     @Override

DerpFest-AOSP/packages_apps_Updater

src/org/lineageos/updater/UpdatesActivity.java
@@ -15,6 +15,7 @@
  */
 package org.lineageos.updater;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
@@ -22,6 +23,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.icu.text.DateFormat;
@@ -45,7 +47,9 @@ import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -88,6 +92,7 @@ public class UpdatesActivity extends UpdatesListActivity {
     private RotateAnimation mRefreshAnimation;
 
     private static final int READ_REQUEST_CODE = 42;
+    private static final int PERMISSION_REQUEST_CODE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -490,12 +495,62 @@ public class UpdatesActivity extends UpdatesListActivity {
     }
 
     private void performFileSearch() {
+        if (checkStoragePermissions()) {
+            return;
+        }
         Intent chooseFile;
         Intent intent;
         chooseFile = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         chooseFile.setType("application/zip");
         intent = Intent.createChooser(chooseFile, "Choose a file");
         startActivityForResult(intent, READ_REQUEST_CODE);
+    }
+
+    private boolean hasStoragePermission() {
+        int result = checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private boolean checkStoragePermissions() {
+        if (hasStoragePermission()) {
+            return false;
+        }
+
+        final String[] perms = {Manifest.permission.READ_EXTERNAL_STORAGE};
+        requestPermissions(perms, PERMISSION_REQUEST_CODE);
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                                != PackageManager.PERMISSION_GRANTED) {
+                            showMessageOKCancel(getString(R.string.dialog_permissions_title),
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                checkStoragePermissions();
+                                            }
+                                        }
+                                    });
+                        }
+                }
+                break;
+        }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton(getString(R.string.dialog_permissions_ask), okListener)
+                .setNegativeButton(getString(R.string.dialog_permissions_dismiss), null)
+                .create()
+                .show();
     }
 
     @Override